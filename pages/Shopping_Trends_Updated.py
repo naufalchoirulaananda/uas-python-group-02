ad8cd5a79febba5b1f4dcd21f269bca9393f3b95
Naufal Choirul Ananda <138176427+naufalchoirulaananda@users.noreply.github.com>
2023-11-06 14:40:20 +0700

    Update Shopping_Trends_Updated.py

--- pages/Shopping_Trends_Updated.py
+++ pages/Shopping_Trends_Updated.py
@@ -4,9 +4,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 from wordcloud import WordCloud, STOPWORDS
-from IPython.display import Image
-import warnings
-warnings.filterwarnings("ignore")
 
 colors = [  "#89CFF0", "#FF69B4", "#FFD700", "#7B68EE", "#FF4500",
             "#9370DB", "#32CD32", "#8A2BE2", "#FF6347", "#20B2AA",
